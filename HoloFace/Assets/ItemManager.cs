6f4d8e88b5f301c5b3c89e7d1c0b9d4becc364ca
PauloHMSa <paulo.miranda107@gmail.com>
2020-03-02 15:51:33 -0300

    Renderizando cabeça e retirando assets Desnecessários

--- HoloFace/Assets/ItemManager.cs
+++ HoloFace/Assets/ItemManager.cs
@@ -1,61 +1,61 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-public class ItemManager : MonoBehaviour//exibe ou esconde a malha da face e o modo debug
-{
-    //ATRIBUTOS::
-    [Tooltip("Shows the attribute values when Debug mode is enabled.")]
-    public Text AttributeText;
+//public class ItemManager : MonoBehaviour//exibe ou esconde a malha da face e o modo debug
+//{
+//    //ATRIBUTOS::
+//    [Tooltip("Shows the attribute values when Debug mode is enabled.")]
+//    public Text AttributeText;
 
-    public GameObject FaceMesh;
+//    public GameObject FaceMesh;
 
-    bool debugMode = false;
-    bool showingFace = false;
-    public bool ShowingFace { get { return showingFace; } }
+//    bool debugMode = false;
+//    bool showingFace = false;
+//    public bool ShowingFace { get { return showingFace; } }
 
-    private Transform transf;
-    //MÉTODOS::
-    void Awake()
-    {
-        Transform[] tempTransforms = FaceMesh.GetComponentsInChildren<Transform>();
-        for (int i = 0; i < tempTransforms.Length; i++)
-        {
-            if (tempTransforms[i].parent == FaceMesh.transform)
-            {
-                transf = tempTransforms[i];
-                break;
-            }
-        }
-#if WINDOWS_UWP
-        HideFace();
-#else
-        ShowFace();
-#endif
-        transf.gameObject.SetActive(true);
-    }
+//    private Transform transf;
+//    //MÉTODOS::
+//    void Awake()
+//    {
+//        Transform[] tempTransforms = FaceMesh.GetComponentsInChildren<Transform>();
+//        for (int i = 0; i < tempTransforms.Length; i++)
+//        {
+//            if (tempTransforms[i].parent == FaceMesh.transform)
+//            {
+//                transf = tempTransforms[i];
+//                break;
+//            }
+//        }
+//#if WINDOWS_UWP
+//        HideFace();
+//#else
+//        ShowFace();
+//#endif
+//        transf.gameObject.SetActive(true);
+//    }
 
-    public void ShowFace()
-    {
-        Renderer renderer = FaceMesh.GetComponent<Renderer>();
-        renderer.enabled = true;
-        showingFace = true;
-    }
+//    public void ShowFace()
+//    {
+//        Renderer renderer = FaceMesh.GetComponent<Renderer>();
+//        renderer.enabled = true;
+//        showingFace = true;
+//    }
 
-    public void HideFace()
-    {
-        Renderer renderer = FaceMesh.GetComponent<Renderer>();
-        renderer.enabled = false;
-        showingFace = false;
-    }
+//    public void HideFace()
+//    {
+//        Renderer renderer = FaceMesh.GetComponent<Renderer>();
+//        renderer.enabled = false;
+//        showingFace = false;
+//    }
 
-    public void ShowDebug() { debugMode = true; }
+//    public void ShowDebug() { debugMode = true; }
 
-    public void HideDebug()
-    {
-        debugMode = false;
-        if (AttributeText != null)
-            AttributeText.text = "";
-    }
-}
+//    public void HideDebug()
+//    {
+//        debugMode = false;
+//        if (AttributeText != null)
+//            AttributeText.text = "";
+//    }
+//}
