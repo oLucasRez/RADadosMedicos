4449cce6c36ec9dd06fff561f4b653ce1de37661
PauloHMSa <paulo.miranda107@gmail.com>
2020-03-05 16:30:27 -0300

    Sliders adicionados

---
+++ HoloFace/Assets/Sliders.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Sliders : MonoBehaviour {
+
+    Renderer rend;
+    public Slider SliderX, SliderY, SliderZ;
+    // Use this for initialization
+    void Start () {
+        rend = GetComponent<Renderer>();
+        rend.material.shader = Shader.Find("VolumeRendering / VolumeRendering");
+	}
+
+    // Update is called once per frame
+    public void UpdateObject() { 
+        for (int i = 0; i < rend.materials.Length; i++)
+        {
+            rend.materials[i].SetVector("_SliceMax", new Vector4(SliderX.value, SliderY.value, SliderZ.value, -1));
+        }
+    }
+
+}
